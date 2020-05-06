2792aaa703945fcd3cffebb9ec447ba234086be1
Alican Demirtas <alican@compositional-it.com>
2020-05-06 21:53:19 +0100

    Sort records with in ascending and descending order for single and multiple colmns.

---
+++ SortRecords.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM HumanResources.Department
+ORDER BY GroupName;
+
+SELECT *
+FROM HumanResources.Department
+ORDER BY GroupName DESC;
+
+SELECT *
+FROM HumanResources.Department
+ORDER BY GroupName DESC, DepartmentID;
